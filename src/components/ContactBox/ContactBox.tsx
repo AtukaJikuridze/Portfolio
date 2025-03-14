a43d411d3abda8737487657e92072729ab5e8daa
Avto <atukajiquridze@gmail.com>
2025-03-14 15:34:48 +0400

    Switched instagram to likeding

--- src/components/ContactBox/ContactBox.tsx
+++ src/components/ContactBox/ContactBox.tsx
@@ -1,5 +1,5 @@
 import "./ContactBox.css";
-import { AiOutlineInstagram, AiOutlineMail } from "react-icons/ai";
+import {  AiOutlineLinkedin, AiOutlineMail } from "react-icons/ai";
 
 export default function ContactBox() {
   return (
@@ -10,8 +10,8 @@ export default function ContactBox() {
         <p>avtojikuridze@gmail.com</p>
       </div>
       <div className="flex-contact-info">
-        <AiOutlineInstagram />
-        <p>jikuridzeatuka</p>
+        <AiOutlineLinkedin  />
+        <p>Avto Jikuridze</p>
       </div>
     </div>
   );
